c6f11d6430e41d1301cea06e05039cbc5eafab7e
Bartek Pacia <barpac02@gmail.com>
2023-12-02 17:12:45 +0100

    add data

--- algo/algo.go
+++ algo/algo.go
@@ -32,23 +32,22 @@ func Init() error {
 	return nil
 }
 
-// / CalcTemp returns I_ost for given I_obl and temp.
+func Calc(I_obl, tolerance float64) (I_ost float64) {
+	I_ost = I_obl / (tolerance * 0.85)
+	return
+}
+
+// CalcTemp returns I_ost for given I_obl and temp.
 func CalcTemp(I_obl, temp float64) (float64, error) {
 	if YDYtolerance == nil {
 		panic(ErrNotInitialized)
 	}
 
-	// load csv and read tolerance for temp
-
 	// start from 1 to skip first column
 	for i := 1; i < len(YDYtolerance); i++ {
-		fmt.Println("checking row", i)
 		thisRow := YDYtolerance[i]
-		// nextRow := YDYtolerance[i+1]
-		// parse int
 		thisTemp, _ := strconv.ParseFloat(thisRow[0], 64)
 		nextTolerance, _ := strconv.ParseFloat(thisRow[1], 64)
-		fmt.Println("nextTolerance: ", nextTolerance, "thisTemp: ", thisTemp)
 		if thisTemp > temp {
 			return Calc(I_obl, nextTolerance), nil
 		}
@@ -57,7 +56,4 @@ func CalcTemp(I_obl, temp float64) (float64, error) {
 	return 0, ErrNotFound
 }
 
-func Calc(I_obl, tolerance float64) (I_ost float64) {
-	I_ost = I_obl / (tolerance * 0.85)
-	return
-}
+func MatchCrossection
