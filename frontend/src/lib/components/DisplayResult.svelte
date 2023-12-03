dee876e1172f950c0bf9b1aa8807c206dbfcc1b7
Mateusz Goik <mateuszgoik1@wp.pl>
2023-12-03 09:22:42 +0100

    Style results table

--- frontend/src/lib/components/DisplayResult.svelte
+++ frontend/src/lib/components/DisplayResult.svelte
@@ -6,6 +6,9 @@
 
 <div class="wrapper">
     <div class="row">
+        <h1>Results:</h1>
+    </div>
+    <div class="row">
         <span>cableType:</span>
         <span>{result.cableType}</span>
     </div>
@@ -29,4 +32,31 @@
         <span>temperature:</span>
         <span>{result.temperature}</span>
     </div>
+    <div class="row link">
+        <a
+            href="https://www.nkt.com.pl/produkty-rozwiazania/niskie-napiecie/przewody-instalacyjne/ydy-450-750-v"
+            target="_blank"
+        >
+            Click here to find this cable
+        </a>
+    </div>
 </div>
+
+<style>
+    .wrapper {
+        width: 100%;
+        padding: 10vh 20vw;
+    }
+
+    .row {
+        font-size: 1.2rem;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 0.5rem;
+    }
+
+    .link {
+        margin-top: 3rem;
+        justify-content: center;
+    }
+</style>
